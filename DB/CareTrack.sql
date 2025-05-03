7a2dae6f578635060ebd46df7a72c27607b6bfc1
GabrielRomero <gabriel.romero5@upr.edu>
2025-05-03 15:43:46 +0000

    sql create statements change

--- DB/CareTrack.sql
+++ DB/CareTrack.sql
@@ -1,13 +1,13 @@
 CREATE TABLE Patients (
-	patient_id int NOT NULL,
+	patient_id int NOT NULL AUTO_INCREMENT,
 	first_name varchar(255) NOT NULL,
-	last_name varchar(255) NOT NULL,
+	last_name varchar(255) NOT NULL ,
 	birth_date DATE,
     PRIMARY KEY (patient_id)
 );
 
 CREATE TABLE Users (
-	user_id int NOT NULL,
+	user_id int NOT NULL AUTO_INCREMENT,
 	first_name varchar(255) NOT NULL,
 	last_name varchar(255) NOT NULL,
 	birth_date DATE,
@@ -17,14 +17,14 @@ CREATE TABLE Users (
 
 
 CREATE TABLE Medical_Conditions(
-	condition_id int NOT NULL, 
+	condition_id int NOT NULL AUTO_INCREMENT, 
     name varchar(255) NOT NULL,
     PRIMARY KEY (condition_id)
 );
 
 
 CREATE TABLE Patient_Condition (
-	patient_condition_id int NOT NULL,
+	patient_condition_id int NOT NULL AUTO_INCREMENT,
     patient_id int NOT NULL,
     condition_id int NOT NULL,
     PRIMARY KEY (patient_condition_id),
@@ -33,7 +33,7 @@ CREATE TABLE Patient_Condition (
 );
 
 CREATE TABLE Annotations (
-    annotation_id int NOT NULL,
+    annotation_id int NOT NULL AUTO_INCREMENT,
     patient_id int NOT NULL,
     created_at DATE,
     user_id int NOT NULL,
