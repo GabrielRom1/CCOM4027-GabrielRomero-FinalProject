b8e3d965da75b478f3ad018735caf9cd88d4056f
GabrielRom1 <gabriel.rome787@gmail.com>
2025-05-07 18:50:48 -0400

    insert and update post. Also get form to edir resource

--- user_insert.php
+++ user_insert.php
@@ -1,66 +1,68 @@
 <?php
     include "connection.php";
 
+    // si edit tiene algo y id tiene algo entonces es que se quiere editar ese user
     if( isset($_GET['edit']) && isset($_GET['id']) ){   
-        echo "get";
+        echo "editar";
         $edit = $_GET['edit'];
         $id = $_GET['id'];
+
+        $id = mysqli_real_escape_string($dbconnection, $_GET['id']);
 
         $query = "SELECT * FROM Users WHERE user_id =".$id;
         
         $result = mysqli_query($dbconnection,$query);
         $datos = mysqli_fetch_array($result);
-    }else{
-        if(isset($_POST['first_name']) && isset($_POST['last_name']) && isset($_POST['birth_date']))  {
+    }elseif(isset($_POST['first_name']) && isset($_POST['last_name']) && isset($_POST['birth_date']) && isset($_POST['role']))  {
+            // si se setean esas variables es un post
+            
             echo "post";
             
             $first_name = $_POST['first_name'];
-            
             $last_name = $_POST['last_name'];
             $birth_date = $_POST['birth_date'];
+            $role = $_POST['role'];
+            $first_name = mysqli_real_escape_string($dbconnection, $first_name);
+            $last_name = mysqli_real_escape_string($dbconnection, $last_name);
+            $birth_date = mysqli_real_escape_string($dbconnection, $birth_date);
+            $role = mysqli_real_escape_string($dbconnection, $role);
 
+            // si se setea el id es que se quiere editar
             if(isset($_POST['id']) ){
                 echo "editar";
-        //         $user_id = $_POST['id'];
+                $user_id = $_POST['id'];
 
-        //         $user_id = mysqli_real_escape_string($dbconnection, $user_id);
-        //         $first_name = mysqli_real_escape_string($dbconnection, $first_name);
-        //         $last_name = mysqli_real_escape_string($dbconnection, $last_name);
-        //         $birth_date = mysqli_real_escape_string($dbconnection, $birth_date);
-            
-        //         $query = "UPDATE Users SET first_name=".$first_name. ", last_name=".$last_name. ", birth_date=". $birth_date. "WHERE user_id="$user_id.";";
+                $user_id = mysqli_real_escape_string($dbconnection, $user_id);
+
+                // $query = "UPDATE Users SET first_name = '{$first_name}';";
+                $query = "UPDATE Users SET first_name = '{$first_name}' , last_name = '{$last_name}' , birth_date = '{$birth_date}', role = '{$role}' where user_id = {$user_id};";
+
+                // $query = "UPDATE Users SET first_name = '".$first_name."' , last_name = '".$last_name. "' , birth_date = '".$birth_date ."' where user_id=1;";
+                echo $query;
+
+                if (mysqli_query($dbconnection,$query)) 
+                {
+                //     $query_delete_emp = "delete from emp where eid=$id_emp";
+                //     mysqli_query($dbconnection,$query_delete_emp);
+                    
+                //     header("Location: demo_struc.php");
+                    echo 'update succesfully';
+            		header("Location: users.php");
+                }	 
             }
-        } 
-        else{
-            echo "crear";
-            // $query = "INSERT INTO Users (first_name, last_name, birth_date, role)";
-            //         echo $query;
-            //         echo 2222;
+            else{
+                echo "crear";
+                $query = "INSERT INTO Users (first_name, last_name, birth_date, role) values ('{$first_name}', '{$last_name}', '{$birth_date}', '{$role}');";
+                // echo $query;
+                // echo "{$query} con f string";
+                if (mysqli_query($dbconnection,$query)){
+                    echo 'create succesfully';
+            		header("Location: users.php");
+                }	
+            }
+        } else{
+            echo "crear2";
         }
-    }
-    // if(isset($_POST['first_name']) && isset($_POST['last_name']) && isset($_POST['birth_date']))  {
-    //     $first_name = $_POST['first_name'];
-    //     $last_name = $_POST['last_name'];
-    //     $birth_date = $_POST['birth_date'];
-
-    //     if(isset($_POST['id']) ){
-    //         $user_id = isset($_POST['id']);
-
-    //         $user_id = mysqli_real_escape_string($dbconnection, $user_id);
-    //         $first_name = mysqli_real_escape_string($dbconnection, $first_name);
-    //         $last_name = mysqli_real_escape_string($dbconnection, $last_name);
-    //         $birth_date = mysqli_real_escape_string($dbconnection, $birth_date);
-            
-    //         $query = "UPDATE Users SET first_name=".$first_name. ", last_name=".$last_name. ", birth_date=". $birth_date. "WHERE user_id="$user_id.";";
-    //     }
-    //     else{
-    //         $query = "INSERT INTO Users (first_name, last_name, birth_date, role)";
-    //     }   
-    // }
-    // else{
-    //     echo 'no query params';
-    // }
-
     $table = 'Users';
 ?>
 
